922e72fff6b1ca881a1d9fad4fb00aba383e992c
Djroton Noé Sossou <70499809+noahdjroton@users.noreply.github.com>
2024-01-22 23:31:06 +0100

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Djroton Noé SOSSOU on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
