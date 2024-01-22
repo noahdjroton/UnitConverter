ff753fdf4799125d8c022ff4630f71c68cd5eba4
Djroton Noé Sossou <70499809+noahdjroton@users.noreply.github.com>
2024-01-23 00:14:48 +0100

    Create an unit converter

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -8,17 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    let unitsList: [UnitLength] = [.meters, .kilometers, .feet, .yards, .miles]
+    @State private var inputUnit: UnitLength = .meters
+    @State private var outputUnit: UnitLength = .kilometers
+    @State private var inputValue: Double = 0
+    
+    var convertedValue: Double {
+        let inputMeasurement = Measurement(value: inputValue, unit: inputUnit)
+        let outputMeasurement = inputMeasurement.converted(to: outputUnit)
+        return Double(outputMeasurement.value)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form{
+            Section("Input unit"){
+                Picker("Select input unit", selection: $inputUnit) {
+                        ForEach(unitsList, id: \.self) { unit in
+                            Text(unit.symbol)
+                        }
+                    }
+                .pickerStyle(.segmented)
+            }
+            
+            Section("Output unit"){
+                Picker("Select output unit", selection: $outputUnit) {
+                        ForEach(unitsList, id: \.self) { unit in
+                            Text(unit.symbol)
+                        }
+                    }
+                .pickerStyle(.segmented)
+            }
+            
+            Section("Input value"){
+                TextField("Input value", value: $inputValue, format: .number)
+            }
+            
+            Section("Out value"){
+                Text(convertedValue, format: .number)
+            }
+
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
-}
